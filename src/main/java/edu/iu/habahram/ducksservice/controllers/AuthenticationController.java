3e7550aec3fc2d2ce03d79db28ff9377b13b4893
daeaker <daeaker@iu.edu>
2024-04-04 12:28:18 -0400

    still errors

--- src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
@@ -2,15 +2,24 @@ package edu.iu.habahram.ducksservice.controllers;
 
 import edu.iu.habahram.ducksservice.model.Customer;
 import edu.iu.habahram.ducksservice.repository.CustomerRepository;
+import edu.iu.habahram.ducksservice.security.TokenService;
+import org.apache.catalina.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
     CustomerRepository customerRepository;
-    public AuthenticationController(CustomerRepository
-                                            customerRepository) {
+    public AuthenticationController(CustomerRepository customerRepository, AuthenticationManager authenticationManager, TokenService tokenService) {
         this.customerRepository = customerRepository;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
     @PostMapping("/signup")
     public void signup(@RequestBody Customer customer) {
@@ -20,4 +29,15 @@ public class AuthenticationController {
             throw new RuntimeException(e);
         }
     }
+
+
+    @PostMapping("/signin")
+    public String login(@RequestBody Customer customer) {
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                customer.username(), customer.password()));
+        return tokenService.generateToken(authentication);
+    }
+
 }
